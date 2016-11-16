80fe66d37211135311fa80a16de3b2a644382a55
Zoe Ames <zoeames@gmail.com>
2016-11-15 18:57:22 -0600

    completed day 1

--- javascripts/app.js
+++ javascripts/app.js
@@ -9,6 +9,7 @@ app.controller("NavCtrl", function($scope){
 app.controller("TodoCtrl", function($scope){
   $scope.welcome = "hello";
   $scope.showListView = true;
+  $scope.newTask = {};
   $scope.items = [
     {
       id: 0,
@@ -39,4 +40,24 @@ app.controller("TodoCtrl", function($scope){
     console.log("you clicked new item");
     $scope.showListView = false;
   }
+
+$scope.addNewItem = function(){
+  $scope.newTask.isCompleted = false;
+  $scope.newTask.id = $scope.items.length;
+console.log("newTask in function", $scope.newTask);
+$scope.items.push($scope.newTask);
+$scope.newTask = {};
+$scope.showListView = true;
+}
+
+
+
+
+
+
+
+
+
+
+
 })
