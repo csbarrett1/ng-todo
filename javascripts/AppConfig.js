662dbb69a3c851030d190277b307d2d0c2380a81
Zoe Ames <zoeames@gmail.com>
2016-11-19 10:35:09 -0600

    added 2 main routes

--- javascripts/AppConfig.js
+++ javascripts/AppConfig.js
@@ -2,4 +2,17 @@
 
 app.run(function(FIREBASE_CONFIG){
   firebase.initializeApp(FIREBASE_CONFIG);
+});
+
+app.config(function($routeProvider){
+	$routeProvider
+		.when('/items/list', {
+			templateUrl: 'partials/item-list.html',
+			controller: 'ItemListCtrl'
+		})
+		.when('/items/new', {
+			templateUrl: 'partials/item-new.html',
+			controller: 'ItemNewCtrl'
+		})
+		.otherwise('/items/list')
 })
